c614f85a17802d4ca48a1976ce2b9c0b8a531dd0
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-02-24 23:34:48 -0800

    Automated Commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import { pageMiddleware } from "./core/middleware";
 import { pageObjects } from "./core/object";
 import { initialization } from "./core/api/index";
 
-const fs = require("fs");
-const dir = "./core";
-
-fs.existsSync(dir)
-  ? console.log("Directory exists")
-  : console.log("Directory Not exists");
+console.log("test");
 
 const katharos = {
   pageActions: pageActions,
