d54080212b208c029f878f01ced191fcac354f0d
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-02-24 22:53:50 -0800

    Automated Commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { pageActions } from "./core/action.js";
 import { pageLoader } from "./core/instance.js";
 import { pageMiddleware } from "./core/middleware.js";
 import { pageObjects } from "./core/object.js";
-import { initialization } from "./api/index.js";
+import { initialization } from "./core/api/index.js";
 
 const katharos = {
   pageActions: pageActions,
