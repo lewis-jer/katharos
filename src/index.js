5cb47a55134c810fa6aa76748fd3733364fa6729
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-02-24 23:39:13 -0800

    Automated Commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { pageMiddleware } from "./core/middleware";
 import { pageObjects } from "./core/object";
 import { initialization } from "./core/api/index";
 
-const fs = require("fs");
-const dir = "./core";
-
-fs.existsSync(dir)
-  ? console.log("Directory exists")
-  : console.log("Directory Not exists");
+try {
+  console.log(require.resolve("katharos-router"));
+} catch (e) {
+  console.error("katharos is not found");
+  process.exit(e.code);
+}
 
 const katharos = {
   pageActions: pageActions,
