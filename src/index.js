8207796d963d59ac80150373893bab0d4a333c94
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-02-25 22:41:34 -0800

    Automated Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { pageActions } from "./core/kdom/action";
 import { pageLoader } from "./core/instance";
-import { pageMiddleware } from "./core/middleware";
 import { pageObjects } from "./core/object";
 import { initialization, plugins } from "./core/api/index";
 
 const katharos = {
   pageActions: pageActions,
   pageLoader: pageLoader,
-  pageMiddleware: pageMiddleware,
   pageObjects: pageObjects,
 };
 
