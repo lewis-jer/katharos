0643a6da6e485a69a9a7771b269287f14e20a580
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-02-25 23:14:12 -0800

    Automated Commit

--- src/core/native/environment/index.js
+++ src/core/native/environment/index.js
@@ -1,10 +1,83 @@
-import { pageLoader, pageReloader } from "./hook";
 import {
   pluginLoader,
   controllerLoader,
   middlewareLoader,
 } from "./environment";
-import { componentLoader } from "./hook-component";
 import { dynamicChartLoader } from "./hook-chart";
+
+const pageLoader = async function (pageInfo) {
+  pageInfo.loadIndex = pageActions.loadIndex;
+  await pluginLoader(pageInfo);
+  await controllerLoader(pageInfo);
+  await middlewareLoader(pageInfo);
+  middleware[pageActions.loadIndex]
+    ? middleware[pageActions.loadIndex]()
+    : false;
+  pageInfo.loaded = true;
+  pageActions.loadIndex++;
+};
+const pageReloader = async function (pageInfo) {
+  middleware[pageInfo.loadIndex]
+    ? await middleware[pageInfo.loadIndex]()
+    : false;
+};
+const componentLoader = async function (pageInfo) {
+  var systemComponents = arrayFunctions.arrayToObject(components.system);
+
+  //Clear Page
+  document.getElementById("wrapper").innerHTML = "";
+
+  //Generate Page Navigation Bar
+  document.getElementById("wrapper").innerHTML += systemComponents.navbar.html;
+
+  componentLib.navigationBar.status = true;
+  componentLib.navigationBar.id = uuid();
+
+  arrayFunctions.arrayToObject(modulePath)[
+    arrayFunctions.arrayToObject(components.system).navbar.arrayExpression
+  ].loaded
+    ? await pageReloader(
+        arrayFunctions.arrayToObject(modulePath)[
+          arrayFunctions.arrayToObject(components.system).navbar.arrayExpression
+        ]
+      )
+    : await pageLoader(
+        arrayFunctions.arrayToObject(modulePath)[
+          arrayFunctions.arrayToObject(components.system).navbar.arrayExpression
+        ]
+      );
+
+  eventMiddleware.addEvent("loadComponent", {
+    componentId: componentLib.navigationBar.id,
+    userIdentifier: JSON.parse(localStorage.getItem("user")).email,
+    location: window.endpoint,
+  });
+
+  //Generate Page Body
+  document.getElementById("content").innerHTML += systemComponents.loader.html;
+
+  componentLib.pageLoader = {};
+  componentLib.pageLoader.status = true;
+  componentLib.pageLoader.id = uuid();
+
+  eventMiddleware.addEvent("loadComponent", {
+    componentId: componentLib.pageLoader.id,
+    userIdentifier: JSON.parse(localStorage.getItem("user")).email,
+    location: window.endpoint,
+  });
+
+  //Generate Page Footer
+  document.getElementById("content").innerHTML += systemComponents.footer.html;
+
+  componentLib.footer = {};
+  componentLib.footer.status = true;
+  componentLib.footer.id = uuid();
+
+  eventMiddleware.addEvent("loadComponent", {
+    componentId: componentLib.footer.id,
+    userIdentifier: JSON.parse(localStorage.getItem("user")).email,
+    location: window.endpoint,
+  });
+};
 
 export { pageLoader, pageReloader, dynamicChartLoader, componentLoader };
