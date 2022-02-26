9debfed9443c1538d1ff0d7967b12117cb902fe6
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-02-25 23:11:04 -0800

    Automated Commit

--- src/core/native/environment/index.js
+++ src/core/native/environment/index.js
@@ -1,3 +1,4 @@
+import { pageLoader, pageReloader } from "./hook";
 import {
   pluginLoader,
   controllerLoader,
@@ -5,23 +6,5 @@ import {
 } from "./environment";
 import { componentLoader } from "./hook-component";
 import { dynamicChartLoader } from "./hook-chart";
-
-const pageLoader = async function (pageInfo) {
-  pageInfo.loadIndex = pageActions.loadIndex;
-  await pluginLoader(pageInfo);
-  await controllerLoader(pageInfo);
-  await middlewareLoader(pageInfo);
-  middleware[pageActions.loadIndex]
-    ? middleware[pageActions.loadIndex]()
-    : false;
-  pageInfo.loaded = true;
-  pageActions.loadIndex++;
-};
-
-const pageReloader = async function (pageInfo) {
-  middleware[pageInfo.loadIndex]
-    ? await middleware[pageInfo.loadIndex]()
-    : false;
-};
 
 export { pageLoader, pageReloader, dynamicChartLoader, componentLoader };
