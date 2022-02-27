4deb8155d7b0337a787326e5a03c441c2e65449f
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-02-27 10:02:41 -0800

    Automated Commit

--- src/core/api/plugin.js
+++ src/core/api/plugin.js
@@ -1,30 +1,9 @@
 let plugins = [];
 try {
-  console.log(require.resolve("katharos-router"));
-  plugins.push("katharos-router");
+  console.log(require.resolve('katharos-router'));
+  plugins.push('katharos-router');
 } catch (e) {
-  console.error("katharos-router is not found");
-}
-
-try {
-  console.log(require.resolve("katharos-dom"));
-  plugins.push("katharos-dom");
-} catch (e) {
-  console.error("katharos-dom is not found");
-}
-
-try {
-  console.log(require.resolve("katharos-event"));
-  plugins.push("katharos-event");
-} catch (e) {
-  console.error("katharos-event is not found");
-}
-
-try {
-  console.log(require.resolve("katharos-data"));
-  plugins.push("katharos-data");
-} catch (e) {
-  console.error("katharos-data is not found");
+  console.error('katharos-router is not found');
 }
 
 export { plugins };
