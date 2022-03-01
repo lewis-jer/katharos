f2170c2f758329b7a723146411a1041645eb8c2c
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-02-28 22:49:59 -0800

    Automated Commit

--- src/core/kdom/dom.js
+++ src/core/kdom/dom.js
@@ -3,6 +3,7 @@ import { modalMiddleware } from './instance/hook-modal';
 import { tableMiddleware } from '../api/helper/hook-table';
 
 const _dom = (_api) => {
+  console.log(_api);
   return {
     ...tableMiddleware(_api),
     ...formMiddleware(_api),
