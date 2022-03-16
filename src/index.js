51dc4f8ee3ea391ff0aea9f94d4d959fba13b66f
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-03-15 22:59:09 -0700

    Automated Commit

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ const katharos = {
     await axios.get(
       'https://fp-exchange.azurewebsites.net/api/trigger-fp-exchange?code=ocU17KflR2/tAPM647FczJtoo60EGbVIeGKKgS7vCkatsrEgmWadeQ==',
       {
-        body: {
-          request: 'secur-katharos-default'
-        }
+        request: 'secur-katharos-default'
       }
     )
   );
