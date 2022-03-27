387ad3bb79cb988a72aab7b89aedc1a343e220e2
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-03-27 16:33:22 -0700

    Automated Commit

--- src/core/api/plugin.js
+++ src/core/api/plugin.js
@@ -8,14 +8,14 @@ try {
 
 const jsAssembler = async (_api, modulePlugin) => {
   await $.getScript(modulePlugin);
-  console.log(`${pluginIndex} => ${modulePlugin}`);
+  //console.log(`${pluginIndex} => ${modulePlugin}`);
   pluginIndex++;
   pluginLib[_api.stringToHash(modulePlugin)] = modulePlugin;
 };
 
 const cssAssembler = async (_api, modulePlugin) => {
   document.head.innerHTML += `<link type="text/css" rel="stylesheet" href=${modulePlugin}?update=${Date.now()}>`;
-  console.log(`${pluginIndex} => ${modulePlugin}`);
+  //console.log(`${pluginIndex} => ${modulePlugin}`);
   pluginIndex++;
   pluginLib[_api.stringToHash(modulePlugin)] = modulePlugin;
 };
