57ee8e24af5ed8de6d9960fab07a9356dc60b330
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-03-27 15:01:52 -0700

    Automated Commit

--- src/core/api/plugin.js
+++ src/core/api/plugin.js
@@ -6,44 +6,38 @@ try {
   console.error('katharos-router is not found');
 }
 
-const jsAssembler = (_api, modulePlugin) => {
+const jsAssembler = async (_api, modulePlugin) => {
   await $.getScript(modulePlugin);
   console.log(`${pluginIndex} => ${modulePlugin}`);
   pluginIndex++;
-  pluginLib[_api.stringToHash(modulePlugin)] = modulePlugin
+  pluginLib[_api.stringToHash(modulePlugin)] = modulePlugin;
 };
 
 const cssAssembler = (_api, modulePlugin) => {
-  document.head.innerHTML += `<link type="text/css" rel="stylesheet" href=${
-    modulePlugin
-  }?update=${Date.now()}>`;
+  document.head.innerHTML += `<link type="text/css" rel="stylesheet" href=${modulePlugin}?update=${Date.now()}>`;
   console.log(`${pluginIndex} => ${modulePlugin}`);
   pluginIndex++;
   pluginLib[_api.stringToHash(modulePlugin)] = modulePlugin;
 };
 
 const assembler = (_api) => {
-  return (modulePlugin) => {
+  return async (modulePlugin) => {
     if (modulePlugin.includes('js')) {
       try {
-        jsAssembler(_api, modulePlugin)
+        await jsAssembler(_api, modulePlugin);
       } catch (e) {
-        console.log(
-          `${pluginIndex} => Failed To Load: ${modulePlugin}`
-        );
+        console.log(`${pluginIndex} => Failed To Load: ${modulePlugin}`);
         pluginIndex++;
       }
     } else if (modulePlugin.includes('css')) {
       try {
-        cssAssembler(_api, modulePlugin)
+        cssAssembler(_api, modulePlugin);
       } catch (e) {
-        console.log(
-          `${pluginIndex} => Failed To Load: ${modulePlugin}`
-        );
+        console.log(`${pluginIndex} => Failed To Load: ${modulePlugin}`);
         pluginIndex++;
       }
     }
-  }
+  };
 };
 
 export { plugins, assembler };
