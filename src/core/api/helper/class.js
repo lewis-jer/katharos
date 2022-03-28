535a20b091bde7cd2577edd73162a47877ed720d
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-03-28 01:33:07 -0700

    Automated Commit

--- src/core/api/helper/class.js
+++ src/core/api/helper/class.js
@@ -72,12 +72,14 @@ while (currentNode) {
 
 class System {
   constructor(data) {
-    this.data = data;
+    this.data = {
+      processName: data.name
+    };
     this.next = null;
   }
 
   setNextNode(node) {
-    if (node instanceof Node || node === null) {
+    if (node instanceof System || node === null) {
       this.next = node;
     } else {
       throw new Error('Next node must be a member of the Node class.');
