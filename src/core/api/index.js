5eedb0089eb39527e4187ab115e1b472918da75d
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-03-28 01:01:17 -0700

    Automated Commit

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -11,18 +11,21 @@ _api = {
   ...helper.tableMiddleware(_api),
   gatherPageInfo: gatherPageInfo(_api)
 };
+console.log(_api.pluginIndex);
 
 _api = {
   ..._api,
   ...helper.modalMiddleware(_api),
   assembler: assembler(_api)
 };
+console.log(_api.pluginIndex);
 
 _api = {
   ..._api,
   ...helper.formMiddleware(_api),
   init: initialization(_api)
 };
+console.log(_api.pluginIndex);
 
 window._katharos_api_ = _api;
 
