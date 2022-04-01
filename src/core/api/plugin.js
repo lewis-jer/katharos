3e1d0ea40e7365bc6bbe9c49b0e509f61d0a9726
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-03-31 23:49:42 -0700

    Automated Commit

--- src/core/api/plugin.js
+++ src/core/api/plugin.js
@@ -15,7 +15,7 @@ const cssAssembler = async (_api, modulePlugin) => {
   _api.system.updatePlugin(modulePlugin);
 };
 
-const assembler = async (_api) => {
+const assembler = (_api) => {
   return async (modulePlugin) => {
     if (modulePlugin.includes('js')) {
       try {
