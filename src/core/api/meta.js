7049c68d2b41b960d5f46dbe5c579f9d39a25af8
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-03-31 20:21:40 -0700

    Automated Commit

--- src/core/api/meta.js
+++ src/core/api/meta.js
@@ -59,17 +59,6 @@ var meta = [
 
 var el = document.createElement('head');
 
-var header = `
-<meta charset='utf-8'> 
-<meta http-equiv='X-UA-Compatible' content='IE=edge'> 
-<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'> 
-<meta name='description' content=''> 
-<meta name='author' content=''> 
-<link rel='shortcut icon' href='https://level.blob.core.windows.net/fp-blob/images/level-favicon.png' type='image/x-icon'/> 
-<title>Level | Official Site</title>`;
-el.innerHTML = header;
-
-var el1 = document.createElement('head');
 for (var i in meta) {
   const child = document.createElement(meta[i].type);
   meta[i].name && child.setAttribute('name', meta[i].name);
@@ -87,10 +76,7 @@ for (var i in meta) {
         child.setAttribute(meta[i].attributes[j], meta[i].values[j]);
   }
   meta[i].container && (child.innerHTML = meta[i].innerHTML);
-  el1.appendChild(child);
+  el.appendChild(child);
 }
-console.log(el1);
-
 console.log(el);
-console.log(el.getElementsByTagName('meta'));
 export { el as meta };
