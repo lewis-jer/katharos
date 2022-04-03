e1d1fb6c4411a3602f7cb39b0458808ea70e7f39
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-04-03 01:11:29 -0700

    Automated Commit

--- src/core/api/helper/class.js
+++ src/core/api/helper/class.js
@@ -23,8 +23,10 @@ class System {
       const entries = Object.fromEntries(new Map([[key, value]]));
       console.log(entries);
       console.log(key.includes('controller'));
-      key.includes('controller') &&
-        Object.assign(this.data.controllerConfig, { entries });
+      (key.includes('controller') &&
+        Object.assign(this.data.controllerConfig, { entries })) ||
+        (key.includes('middleware') &&
+          Object.assign(this.data.controllerConfig, { entries }));
     }
   };
 
