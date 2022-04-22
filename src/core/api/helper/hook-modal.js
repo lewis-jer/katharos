81f61e9e2c42993b5ee5e1a969e6da6d0888358f
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-04-22 10:52:12 -0700

    Automated Commit

--- src/core/api/helper/hook-modal.js
+++ src/core/api/helper/hook-modal.js
@@ -30,7 +30,7 @@ const addElementsById = (_api) => {
       for (var i in newObjects) {
         modalObjects.push(newObjects[i]);
         modalInfo.inputStoreSession.push(newObjects[i][0]);
-        odalInfo.inputDataStoreSession.push(newObjects[i][1]);
+        modalInfo.inputDataStoreSession.push(newObjects[i][1]);
       }
     }
 
