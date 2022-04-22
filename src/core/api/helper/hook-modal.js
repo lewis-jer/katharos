34afcc6cd8987643a841f910484ce688c18aa6cf
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-04-21 23:09:10 -0700

    Automated Commit

--- src/core/api/helper/hook-modal.js
+++ src/core/api/helper/hook-modal.js
@@ -54,7 +54,7 @@ const removeElementsById = (_api) => {
 
 const modalSync = (_api) => {
   return (modalFunc, modalName) => {
-    console.log(modalFunc, modalName);
+    console.log(_api.system.getModal(modalName));
     for (var i in modals[modalFunc]) {
       if (modals[modalFunc][i].modal == modalName) {
         for (var j in modals[modalFunc][i].select) {
