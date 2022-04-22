84b5cd185e31618b2e879634a1d08bfbd0e49147
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-04-21 22:57:20 -0700

    Automated Commit

--- src/core/api/helper/class.js
+++ src/core/api/helper/class.js
@@ -30,7 +30,7 @@ class System {
 
       key.includes('secret') && this.setSecureContainer(value);
 
-      key.includes('modals') && value.forEach((x) => console.log(x));
+      key.includes('modals') && console.log(value);
 
       key.includes('forms') && Object.assign(this.data.forms, { ...value });
     }
