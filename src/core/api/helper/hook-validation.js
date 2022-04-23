30d59595e8f3344b6e0065426ecacf1e27c1fd09
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-04-22 22:00:51 -0700

    Automated Commit

--- src/core/api/helper/hook-validation.js
+++ src/core/api/helper/hook-validation.js
@@ -18,8 +18,8 @@ const validateFormData = (_api) => {
       const [key, value] = entry;
       form.encryption.includes(key) && (data[key] = _api.encrypter(value));
     });
-    console.log(typeof data.SN !== 'undefined');
-    typeof data.SN !== 'undefined' && (data.SN = uuid());
+    console.log(typeof data.SN == 'undefined');
+    typeof data.SN == 'undefined' && (data.SN = uuid());
     console.log(data);
 
     return data;
