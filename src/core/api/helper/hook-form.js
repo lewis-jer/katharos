9a15916f0ecdd2fefcbc12e7376bfd107043ab55
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-04-22 20:56:41 -0700

    Automated Commit

--- src/core/api/helper/hook-form.js
+++ src/core/api/helper/hook-form.js
@@ -98,7 +98,7 @@ const formSubmit = (_api) => {
   return async (contents, formName, formAction, modalName, tableName) => {
     var endpoint = modalName.replace(`${formAction}`, '');
     var data = parseFormData(contents, formAction);
-    data1 = validateFormData(_api)(modalName, data);
+    var data1 = validateFormData(_api)(modalName, data);
     console.log(data1);
     if (endpoint == 'tx') {
       data = [data];
@@ -120,18 +120,18 @@ const formSubmit = (_api) => {
     let res;
     if (formAction == 'add') {
       if (endpoint == 'tx') {
-        await dataService('POST', endpoint, false, data).then(
-          async ({ data: res }) => {
-            if (res.error) {
-              completeAction(_api)(formName, formAction, modalName);
-              alertify.error(res.error);
-            }
-            data.id = res.insertId;
-            await _api.updateTable(tableName, data, formAction, endpoint);
-            completeAction(_api)(formName, formAction, modalName);
-            alertify.success('Success message');
-          }
-        );
+        // await dataService('POST', endpoint, false, data).then(
+        //   async ({ data: res }) => {
+        //     if (res.error) {
+        //       completeAction(_api)(formName, formAction, modalName);
+        //       alertify.error(res.error);
+        //     }
+        //     data.id = res.insertId;
+        //     await _api.updateTable(tableName, data, formAction, endpoint);
+        //     completeAction(_api)(formName, formAction, modalName);
+        //     alertify.success('Success message');
+        //   }
+        // );
       } else if (endpoint == 'bcat') {
         data.func = document.getElementById('el1').innerHTML;
         data.bcat = uuid();
