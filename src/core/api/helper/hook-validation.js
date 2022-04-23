b25d710be406300cd58a95afc37db8c2c2ab1ca0
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-04-22 21:06:52 -0700

    Automated Commit

--- src/core/api/helper/hook-validation.js
+++ src/core/api/helper/hook-validation.js
@@ -9,7 +9,32 @@ const parseFormData = (contents, formAction) => {
 
 const validateFormData = (_api) => {
   return (modalName, data) => {
+    var modal = _api.system.getModal(modalName);
     console.log(data);
+
+    data = [data];
+
+    data.forEach((x) => {
+      data.push({
+        tx: _api.encrypter(x.tx),
+        txdate: x.txdate,
+        txamount: x.txamount,
+        txbcat: x.txbcat,
+        txdesc: '',
+        username: userProfile.username
+      });
+    });
+
+    data.splice(0, 1);
+
+    data = data[0];
+
+    data.SN = typeof data.SN !== 'undefined' ? data.SN : uuid();
+
+    console.log(data);
+
+    modal.encryption;
+
     return data;
   };
 };
