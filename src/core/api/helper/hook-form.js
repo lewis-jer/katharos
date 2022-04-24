ef21f6cb9451e1f3af2a2cb3e92cf91fecb3ea18
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-04-24 02:08:09 -0700

    Automated Commit

--- src/core/api/helper/hook-form.js
+++ src/core/api/helper/hook-form.js
@@ -120,7 +120,9 @@ const formSubmit = (_api) => {
     var data = parseFormData(contents, formAction);
     form.version == 1 && (data = validateFormData(_api)(form, data));
     form.store && Object.assign(data, { ..._api.store.getInputStore() });
-
+    console.log('----------------------');
+    console.log(data);
+    console.log('----------------------');
     if (form.enabled) {
       const response =
         form.version == 1 && (await submissionHandle(form.handle, data));
@@ -143,6 +145,7 @@ const formSubmit = (_api) => {
         data.bcat = uuid();
         data.SN = uuid();
         data.username = userProfile.username;
+        console.log(data);
         // await dataService('POST', endpoint, false, data).then(
         //   async ({ data: res }) => {
         //     if (res.error) {
