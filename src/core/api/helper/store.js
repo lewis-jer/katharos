d5c38b04e2c3bd5c6ed2de9f80824671c498a688
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-05-21 21:31:06 -0700

    Automated Commit

--- src/core/api/helper/store.js
+++ src/core/api/helper/store.js
@@ -7,20 +7,36 @@ class Store {
     this.next = null;
   }
 
-  setInputItem(data) {
-    Object.assign(this.data.inputStore, { ...Object.fromEntries(data) });
+  getInputStore() {
+    return this.data.inputStore;
   }
 
   getInputItem(key) {
     return this.data.inputStore[key];
   }
 
+  setInputItem(data) {
+    Object.assign(this.data.inputStore, { ...Object.fromEntries(data) });
+  }
+
   setInputStoreItem(key, value) {
     this.data.inputStore[key] = value;
   }
 
-  getInputStore() {
-    return this.data.inputStore;
+  getDomStore() {
+    return this.data.domStore;
+  }
+
+  getDomItem(key) {
+    return this.data.domStore[key];
+  }
+
+  setDomItem(data) {
+    Object.assign(this.data.domStore, { ...Object.fromEntries(data) });
+  }
+
+  setDomStoreItem(key, value) {
+    this.data.domStore[key] = value;
   }
 
   clearInputStore() {
