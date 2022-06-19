ad22eaca306ed48ee31b4a3476c17c6622b746e4
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-06-18 22:58:16 -0700

    Automated Commit

--- src/core/kdom/action-canvas.js
+++ src/core/kdom/action-canvas.js
@@ -6,8 +6,6 @@ const loadPage = (_api) => {
   return async (currPage, pageName) => {
     let router = await getEndpoint(_api, currPage, pageName);
     let page = _api.system.getView(currPage);
-    console.log(currPage, pageName);
-    console.log(page);
     if (router.sourceRouteInformation.loaded) {
       _api.addEvent('clearPage', {
         documentId: page.id,
@@ -19,11 +17,9 @@ const loadPage = (_api) => {
 
     window.endpoint = router.route;
     let destination = _api.system.getView(router.route);
-    console.log(router.route);
-    console.log(destination);
     await generatePage(router.route, router.routeInformation);
     _api.addEvent('generatePage', {
-      documentId: documents[router.route].id,
+      documentId: destination.id,
       userIdentifier: router.authentication.userId,
       target: router.route
     });
