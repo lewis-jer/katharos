1496e4afcb9c490e6c0805d4c5549758b22a6f81
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-06-19 04:01:49 -0700

    Automated Commit

--- src/core/native/environment/index.js
+++ src/core/native/environment/index.js
@@ -1,6 +1,6 @@
 import { dynamicChartLoader } from './hook-chart';
 
-async function pageLoader(_api, pageInfo) {
+const pageLoader = async function (_api, pageInfo) {
   console.log('pageLoader: ', this);
   pageInfo.loadIndex = configuration.katharos.pageActions.loadIndex;
   for (var i in pageInfo.plugins) {
@@ -13,11 +13,11 @@ async function pageLoader(_api, pageInfo) {
     .then((res) => console.log(res));
   pageInfo.loaded = true;
   configuration.katharos.pageActions.loadIndex++;
-}
-async function pageReloader(_api, pageInfo) {
+};
+const pageReloader = async function (_api, pageInfo) {
   console.log('pageReloader: ', this);
   await _api.system.instantiateMiddleware(_api, pageInfo);
-}
+};
 const componentLoader = async function (_api, pageInfo) {
   var components = _api.system.getComponents();
   console.log(components);
