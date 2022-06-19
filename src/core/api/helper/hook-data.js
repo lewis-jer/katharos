8abaca59cc5fa42409ecec8fee79e4247e02ca1d
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-06-18 21:45:54 -0700

    Automated Commit

--- src/core/api/helper/hook-data.js
+++ src/core/api/helper/hook-data.js
@@ -63,12 +63,15 @@ const dataHandler = {
   plaid: async function ($) {
     console.log(this);
     const fetchLinkToken = async () => {
-      // console.log(
-      //   await this.system
-      //     .http()
-      //     .get('plaid/create-link-token/' + this.user.getUsername())
-      // );
-      const { linkToken } = await dataService('GET', 'plaid/create-link-token');
+      console.log(
+        await this.system
+          .http()
+          .get('plaid/create-link-token/' + this.user.getUsername())
+      );
+      const { linkToken } = await dataService(
+        'GET',
+        'fp-app/plaid/create-link-token'
+      );
       return linkToken;
     };
 
