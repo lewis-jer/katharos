f54176d86d9e0ad4519430c0372462b8a6a4e8a1
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-06-19 03:58:03 -0700

    Automated Commit

--- src/core/native/environment/index.js
+++ src/core/native/environment/index.js
@@ -1,6 +1,6 @@
 import { dynamicChartLoader } from './hook-chart';
 
-const pageLoader = async function (_api, pageInfo) {
+async function pageLoader(_api, pageInfo) {
   console.log('pageLoader: ', this);
   pageInfo.loadIndex = configuration.katharos.pageActions.loadIndex;
   for (var i in pageInfo.plugins) {
@@ -13,11 +13,11 @@ const pageLoader = async function (_api, pageInfo) {
     .then((res) => console.log(res));
   pageInfo.loaded = true;
   configuration.katharos.pageActions.loadIndex++;
-};
-const pageReloader = async function (_api, pageInfo) {
+}
+async function pageReloader(_api, pageInfo) {
   console.log('pageReloader: ', this);
   await _api.system.instantiateMiddleware(_api, pageInfo);
-};
+}
 const componentLoader = async function (_api, pageInfo) {
   var components = _api.system.getComponents();
   console.log(components);
@@ -43,7 +43,6 @@ const componentLoader = async function (_api, pageInfo) {
 
   //Generate Page Body
   document.getElementById('content').innerHTML += components.loader.html;
-
   _api.system.componentLoader('pageLoader', true);
   _api.addEvent('loadComponent', {
     componentId: _api.system.getComponentId('pageLoader'),
@@ -53,7 +52,6 @@ const componentLoader = async function (_api, pageInfo) {
 
   //Generate Page Footer
   document.getElementById('content').innerHTML += components.footer.html;
-
   _api.system.componentLoader('footer', true);
   _api.addEvent('loadComponent', {
     componentId: _api.system.getComponentId('footer'),
