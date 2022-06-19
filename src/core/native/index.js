6bba0f5071fcb0a6d1f94c957b48aee8a9506555
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-06-19 12:49:33 -0700

    Automated Commit

--- src/core/native/index.js
+++ src/core/native/index.js
@@ -1,12 +1,14 @@
 import { drawPage, dynamicTableDestructor, pageDestructor } from './render.js';
 
 const generate = (_api) => {
+  console.log('generate', this);
   return async (pageName, pageInfo) => {
     await drawPage(pageName, pageInfo, _api);
   };
 };
 
 const clearPage = async (_api, pageInfo) => {
+  console.log('clearPage', this);
   await dynamicTableDestructor(_api, pageInfo);
   await pageDestructor(pageInfo);
 };
