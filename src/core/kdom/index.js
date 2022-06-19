a04b9eee7ee866b054f706077ce54dcce2286d20
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-06-19 15:56:37 -0700

    Automated Commit

--- src/core/kdom/index.js
+++ src/core/kdom/index.js
@@ -2,6 +2,7 @@ import { loadPage } from './action-canvas';
 
 function pageActions(_api) {
   console.log('pageActions: ', this);
+  console.log('pageActions: ', _api);
   return {
     loadPage: loadPage(_api),
     loadIndex: 1
