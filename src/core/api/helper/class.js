0731ecfb570c82203c16fbb85730d1fda615fa19
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-06-18 22:26:46 -0700

    Automated Commit

--- src/core/api/helper/class.js
+++ src/core/api/helper/class.js
@@ -54,6 +54,7 @@ class System {
       }
 
       if (key.includes('views')) {
+        console.log(value);
         for (const [module, view] of Object.entries(value)) {
           this.data.views[view.name] = view;
         }
