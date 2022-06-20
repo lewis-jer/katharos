fe4012ed9dc356615949701665a8940f760c398c
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-06-19 18:25:44 -0700

    Automated Commit

--- src/core/api/init.js
+++ src/core/api/init.js
@@ -1,14 +1,15 @@
 const initialization = (_api) => {
   return async function (url) {
-    for (var i in modulePath) {
-      modulePath[i].arrayExpression = modulePath[i].endpoint;
-      if (modulePath[i].system) {
-        modulePath[i].loaded = true;
-        modulePath[i].loadIndex = 0;
+    modulePath = _api.system.getModules();
+    for (const current in modulePath) {
+      current.arrayExpression = current.endpoint;
+      if (current.system) {
+        current.loaded = true;
+        current.loadIndex = 0;
         await _api.system.initializeController('system reserved');
         await _api.system.initializeMiddleware('system reserved');
-        for (var j in modulePath[i].plugins) {
-          await _api.assembler(modulePath[i].plugins[j]);
+        for (var j in current.plugins) {
+          await _api.assembler(current.plugins[j]);
         }
       }
     }
