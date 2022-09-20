8995a2043d31a3f0bbacedb8eb644786ea44099a
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-09-19 19:10:55 -0700

    Automated Commit

--- src/core/api/helper/hook-form.js
+++ src/core/api/helper/hook-form.js
@@ -167,10 +167,8 @@ function formHelperAction(_api) {
           form.version == 1 &&
           (await (async () => {
             const { data: res } = response;
-            console.log(res.insertId);
-            console.log(res.insertId != 0)(
-              typeof res.insertId !== 'undefined' || res.insertId != 0
-            ) && (data.id = res.insertId);
+            (typeof res.insertId !== 'undefined' || res.insertId == 0) &&
+              (data.id = res.insertId);
             const params = { form, response, data, tableName };
             console.log('Inside Scope: ', this);
             if (form.action == 'block') {
