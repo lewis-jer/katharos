a890b43857af485bb21264d77b6aa6d2480ad9a7
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-09-28 18:55:04 -0700

    Automated Commit

--- src/core/api/helper/hook-form.js
+++ src/core/api/helper/hook-form.js
@@ -56,15 +56,15 @@ function formHelperAction(_api) {
       console.log(array, value);
       var duplicates = [];
       return array.filter((data) => {
-        console.log(data);
         console.log(
+          data,
           JSON.stringify(data).toLowerCase().indexOf(value.toLowerCase())
         );
         if (!duplicates.includes(data)) {
           duplicates.push(data);
           return (
             JSON.stringify(data).toLowerCase().indexOf(value.toLowerCase()) !==
-            -1
+              -1 || JSON.stringify(data).toLowerCase() !== 'namedItem'
           );
         }
         console.log('Line 70: ', data);
