5c40ac0827e8067197cfea14c081674fdae81b7b
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-10-27 21:30:33 -0700

    Automated Commit

--- src/core/api/helper/hook-form.js
+++ src/core/api/helper/hook-form.js
@@ -28,8 +28,6 @@ function formHelperAction(_api) {
         return true;
       } else if (_api.user.getUserStatus()) {
         return true;
-      } else if (!_api.user.getUserStatus() && _api.user.getUserCount() == this.count) {
-        return true;
       }
 
       return false;
