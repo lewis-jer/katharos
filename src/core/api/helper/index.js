cdb314c0e140bf59dd968673d10ff00a45152466
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-11-06 00:08:23 -0700

    Automated Commit

--- src/core/api/helper/index.js
+++ src/core/api/helper/index.js
@@ -1,6 +1,5 @@
 import { tableMiddleware } from './hook-table';
 import { formHelperAction } from './hook-form';
-import { modalMiddleware } from './hook-modal';
 import { dataHandler } from './hook-data';
 
 const event_log = (window.event_log = []);
